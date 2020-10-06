da54789fc1fa2d8699692f23a03878337dfc2d2f
Tau-Tek <w.owen.wood@gmail.com>
2020-10-05 20:37:34 -0700

    Update AddingMachine.java

--- AddingMachine.java
+++ AddingMachine.java
@@ -7,40 +7,54 @@ package cse360assignment02;
 public class AddingMachine {
   /**
    * the total value
+   * accessed with getTotal()
   */
   private int total;
+  /**
+   * the history of the adding machine. starts as "0", adding value appends " + value", subtracting value appends " + value"
+   * accessed with toString()
+  */
+  private string history;
   
   public AddingMachine () {
     total = 0;  // not needed - included for clarity
+	history = "0";
   }
   /**
    * get the total value of the adding machine
    * @return the total value of the adding machine
   */
   public int getTotal () {
-    return 0;
+    return total;
   }
   /**
    * add a value to the total of the adding machine
    * @param value the value to be added to the total
   */
   public void add (int value) {
+	  total += value;
+	  history = history + " + " + value;
   }
   /**
    * subtract a value from the total of the adding machine
    * @param value the value to be subtract from the total
   */
   public void subtract (int value) {
+	  total -= value;
+	  history = history + " - " + value;
   }
   /**
-   * 
+   * prints the history of the adding machine
+   * @return history of adding machine
   */
   public String toString () {
-    return "";
+    return history;
   }
   /**
-   *
+   * resets the counting machine, setting total to 0 and history to "0"
   */
   public void clear() {
+	  total = 0;
+	  history = "0";
   }
 }
