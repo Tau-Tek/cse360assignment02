07558feb74cffd1d98f4c46150a2ee5d8b781802
Tau-Tek <w.owen.wood@gmail.com>
2020-10-05 19:07:02 -0700

    add addingmachine

---
+++ AddingMachine.java
@@ -0,0 +1,46 @@
+package cse360assignment02;
+
+/**
+ * Describes an object that has a value that can be added to and subtracted from
+ * The counting machine keeps a history of transactions
+ */
+public class AddingMachine {
+  /**
+   * the total value
+  */
+  private int total;
+  
+  public AddingMachine () {
+    total = 0;  // not needed - included for clarity
+  }
+  /**
+   * get the total value of the adding machine
+   * @return the total value of the adding machine
+  */
+  public int getTotal () {
+    return 0;
+  }
+  /**
+   * add a value to the total of the adding machine
+   * @param value the value to be added to the total
+  */
+  public void add (int value) {
+  }
+  /**
+   * subtract a value from the total of the adding machine
+   * @param value the value to be subtract from the total
+  */
+  public void subtract (int value) {
+  }
+  /**
+   * 
+  */
+  public String toString () {
+    return "";
+  }
+  /**
+   *
+  */
+  public void clear() {
+  }
+}
